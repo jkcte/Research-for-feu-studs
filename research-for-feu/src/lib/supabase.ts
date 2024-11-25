90a3172f90b5d6ec0c6048310adf2c008d4644dd
jkcte <124320257+jkcte@users.noreply.github.com>
2024-11-25 08:52:41 +0800

    for now

--- research-for-feu/src/lib/supabase.ts
+++ research-for-feu/src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = "https://dmxsmppnlxiczcuimpba.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRteHNtcHBubHhpY3pjdWltcGJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5NjY2MDQsImV4cCI6MjA0NzU0MjYwNH0.ZB767mBe1C6gOBYAXTbWjylD5DpF0Nyz5U98L_rDVeE"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
